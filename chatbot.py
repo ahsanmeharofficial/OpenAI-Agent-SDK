d1660f635f665bd4b75cccb440b28a94f6eb7651
engrmianahsan <ahsanmeharofficial01@gmail.com>
2025-09-04 15:08:04 +0500

    Initial implementation of the project structure and basic functionality.

---
+++ chatbot.py
@@ -0,0 +1,6 @@
+import chainlit as cl
+@cl.on_message
+async def main(message: cl.Message):
+    # Our custom logic goes here...
+    # Send a fake response back to the user
+    await cl.Message(content=f"You said: {message.content}").send()
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Code with Ahsan Mehar")
+
+
+if __name__ == "__main__":
+    main()
